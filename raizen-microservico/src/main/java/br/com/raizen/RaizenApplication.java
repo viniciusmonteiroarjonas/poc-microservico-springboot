1866655ba325ff4776b3c8cedc91369db4732262
vinicius monteiro arjonas <viniciusmonteiroarjonas@outlook.com>
2023-04-20 21:38:15 -0300

    commit inicial configuração projeto

---
+++ raizen-microservico/src/main/java/br/com/raizen/RaizenApplication.java
@@ -0,0 +1,13 @@
+package br.com.raizen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizenApplication.class, args);
+	}
+
+}
